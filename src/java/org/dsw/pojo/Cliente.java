1ad886abb3c964ad4bf4b35b822ca21fbe233da0
CAIO <caio.b.santos@hotmail.com>
2019-05-21 11:01:18 -0300

    pojo Cliente

---
+++ src/java/org/dsw/pojo/Cliente.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dsw.pojo;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+
+
+
+@Entity
+@NamedQueries({@NamedQuery(name = "Cliente.findAll", query = "SELECT u FROM Cliente u"),
+        @NamedQuery(name = "Cliente.findByCPF", query = "SELECT u FROM Cliente u WHERE u.CPF = :CPF")})
+public class Cliente implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
+    private String nome;
+    private String email;
+    private String senha;
+    private String CPF;
+    private String telefone;
+    private String sexo;
+    private String dataNasc;
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+    
+    public String getSenha(){
+        return this.senha;
+    }
+    
+    public void setCPF(String CPF){
+        this.CPF = CPF;
+    }
+    
+    public String getCPF(){
+        return this.CPF;
+    }
+    
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    
+    public String getTelefone(){
+        return this.telefone;
+    }
+    
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+    
+    public String getSexo(){
+        return this.sexo;
+    }
+    
+    public void setDataNasc(String dataNasc){
+        this.dataNasc = dataNasc;
+    }
+    
+    public String getDataNasc(){
+        return this.dataNasc;
+    }
+}
