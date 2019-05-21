a989f2136c747d309826755a0d77f9cf1431e14a
CAIO <caio.b.santos@hotmail.com>
2019-05-21 11:05:38 -0300

    Locadora Pojo

---
+++ src/java/org/dsw/pojo/Locadora.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dsw.pojo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({@NamedQuery(name = "Locadora.findAll", query = "SELECT u FROM Locadora u"),
+        @NamedQuery(name = "Locadora.findByCNPJ", query = "SELECT u FROM Locadora u WHERE u.CNPJ = :CNPJ")})
+public class Locadora implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
+    private String nome;
+    private String email;
+    private String senha;
+    private String CNPJ;
+    private String cidade;
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+    
+    public String getSenha(){
+        return this.senha;
+    }
+    
+    public void setCNPJ(String CNPJ){
+        this.CNPJ = CNPJ;
+    }
+    
+    public String getCNPJ(){
+        return this.CNPJ;
+    }
+    
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+    
+    public String getCidade(){
+        return this.cidade;
+    }
+}
