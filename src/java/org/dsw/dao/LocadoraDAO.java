fc4a067d75257dad9f103365d226b4a4a311efd7
Gabriel Nardy <gtnardy@gmail.com>
2019-05-27 13:27:59 -0300

    Criação do DAO da Locadora

---
+++ src/java/org/dsw/dao/LocadoraDAO.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dsw.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import org.dsw.pojo.Locadora;
+
+
+
+public class LocadoraDAO extends GenericDAO<Locadora>{
+
+    @Override
+    public Locadora get(int id) {
+        EntityManager em = this.getEntityManager();
+        Locadora locadora = em.find(Locadora.class, id);
+        em.close();
+        return locadora;
+    }
+
+    @Override
+    public List<Locadora> getAll() {
+        EntityManager em = this.getEntityManager();
+        Query q = em.createQuery("select l from Locadora l", Locadora.class);
+        List<Locadora> locadoras = q.getResultList();
+        em.close();
+        return locadoras; 
+    }
+
+    @Override
+    public void insert(Locadora locadora) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(locadora);
+        tx.commit();
+        em.close();
+    }
+
+    @Override
+    public void update(Locadora locadora) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(locadora);
+        tx.commit();
+        em.close();
+    }
+
+    @Override
+    public void delete(Locadora locadora) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        locadora = em.getReference(Locadora.class, locadora.getId());
+        tx.begin();
+        em.remove(locadora);
+        tx.commit();
+    }
+    
+}
